9cda5c25bbdaa694431139c05a6cd59dd78ed6f9
Matthew de Beer <matthewdb85@gmail.com>
2019-06-27 17:57:01 +0200

    Added V1 of the task itself

---
+++ Task/TaskV1/index.ts
@@ -0,0 +1,23 @@
+import taskLibrary = require('azure-pipelines-task-lib/task');
+import shell = require('shelljs');
+import operatingSystem = require('os');
+import { CommandLineGitClient } from './GitClient/CommandLineGitClient';
+import { GitTagExtractor } from './TagExtractor/GitTagExtractor';
+
+async function run() {
+    // TODO: Test if this works on Windows and Linux hosted agents
+    var separator = decodeURI('%0A');
+    
+    var gitClient = new CommandLineGitClient(shell.which, shell.exec);
+    var gitTagExtractor = new GitTagExtractor(gitClient, separator);
+
+    try {
+        var tag = gitTagExtractor.getGitTag();
+        taskLibrary.setVariable('ExtractedGitTag', tag);
+        console.log(tag);
+    } catch (error) {
+        taskLibrary.setResult(taskLibrary.TaskResult.Failed, error.message);
+    }
+}
+
+run();
