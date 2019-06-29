28d375efed7eb85de1bfabb905727897f52c911a
Matthew de Beer <matthewdb85@gmail.com>
2019-06-30 00:19:25 +0200

    Added GitTagExtractor tests

---
+++ Task/tests/_suite.ts
@@ -0,0 +1,7 @@
+import GitTagExtractorTests from './TaskV1/TagExtractor/GitTagExtractorTests';
+
+describe('Task tests', function() {
+    describe('Version 1', function() {
+        describe('Tag Extractor', GitTagExtractorTests.bind(this));
+    });
+});
