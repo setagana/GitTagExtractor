900f5f63add270500da7e9f27dc9e72a72748f78
Matthew de Beer <matthewdb85@gmail.com>
2019-06-30 12:43:52 +0200

    Added git client tests

--- Task/tests/_suite.ts
+++ Task/tests/_suite.ts
@@ -1,7 +1,9 @@
 import GitTagExtractorTests from './TaskV1/TagExtractor/GitTagExtractorTests';
+import CommandLineGitClientTests from './TaskV1/GitClient/CommandLineGitClientTests';
 
 describe('Task tests', function() {
     describe('Version 1', function() {
         describe('Tag Extractor', GitTagExtractorTests.bind(this));
+        describe('Command Line Git Client', CommandLineGitClientTests.bind(this));
     });
 });
