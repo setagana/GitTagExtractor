f47223b55d8f5338294e1ddf708e181d64451285
Matthew de Beer <matthewdb85@gmail.com>
2019-07-01 12:11:59 +0200

    Moved around a ton of stuff

--- Task/tests/_suite.ts
+++
@@ -1,9 +0,0 @@
-import GitTagExtractorTests from './taskv1/TagExtractor/GitTagExtractorTests';
-import CommandLineGitClientTests from './taskv1/GitClient/CommandLineGitClientTests';
-
-describe('Task tests', function() {
-    describe('Version 1', function() {
-        describe('Tag Extractor', GitTagExtractorTests.bind(this));
-        describe('Command Line Git Client', CommandLineGitClientTests.bind(this));
-    });
-});
