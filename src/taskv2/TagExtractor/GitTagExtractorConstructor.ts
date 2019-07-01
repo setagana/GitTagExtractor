d87ea1e2e32c0f9730e2cbef5b8ce7734a5d6419
Matthew de Beer <matthewdb85@gmail.com>
2019-07-01 16:53:02 +0200

    Implemented use of output option

--- src/taskv2/TagExtractor/GitTagExtractorConstructor.ts
+++ src/taskv2/TagExtractor/GitTagExtractorConstructor.ts
@@ -1,6 +1,7 @@
 import { GitClientInterface } from '../GitClient/GitClientInterface';
 import { GitTagExtractorInterface } from './GitTagExtractorInterface';
+import { OutputOptions } from '../output-options';
 
 export interface GitTagExtractorConstructor {
-    new (gitClient: GitClientInterface, separator: string) : GitTagExtractorInterface;
+    new (gitClient: GitClientInterface, separator: string, output: OutputOptions) : GitTagExtractorInterface;
 }
---
+++ src/taskv2/output-options.ts
@@ -0,0 +1,4 @@
+export enum OutputOptions {
+    AllTags,
+    SemanticVersionTagOnly
+}
